20e9896546104ecd1b804e0d33429cc359297971
merona0531 <moonpower0531@naver.com>
2025-01-20 20:05:32 +0900

    로그인 메인 개발

---
+++ src/pages/login/login.js
@@ -0,0 +1,34 @@
+import {
+    Wrapper, Logo, LogInWrapper, RightImg, LogInInput,
+    LogInButton, SignInButton, FindButton,
+    LogInUnderWrapper, ImgWrapper, LogoWrapper, MiddleWrapper
+} from './loginstyle'
+import React from "react";
+import { Reset } from 'styled-reset';
+import Image from '../../images/forloginpage.png'
+import LogoImage from '../../images/img.png'
+
+export default function LoginPage(){
+
+    return(
+        <>
+                <Wrapper>
+                    <LogoWrapper>
+                        <Logo src={LogoImage}></Logo>
+                    </LogoWrapper>
+                    <MiddleWrapper>
+                        <LogInWrapper>
+                            <LogInInput type="text" maxLength="20" size="44" placeholder="아이디"  />
+                            <LogInInput type="password" maxLength="20" size="44" placeholder="비밀번호" />
+                            <LogInButton type="button" >로그인</LogInButton>
+                            <LogInUnderWrapper>
+                                <SignInButton >회원가입</SignInButton>
+                                <FindButton>아이디·비밀번호 찾기</FindButton>
+                            </LogInUnderWrapper>
+                        </LogInWrapper>
+                        <ImgWrapper><RightImg src={Image}></RightImg></ImgWrapper>
+                    </MiddleWrapper>
+                </Wrapper>
+        </>
+    )
+}
---
+++ src/pages/login/loginstyle.js
@@ -0,0 +1,115 @@
+import styled from 'styled-components';
+
+export const Wrapper=styled.div`
+    height: 96vh;
+    flex-direction: column;
+    overflow: hidden;
+    @media (max-width: 1000px) {
+        height: 95vh;
+    }
+`
+export const LogoWrapper = styled.div`
+    width: auto;
+    height: 80px;
+    padding-left: 10%;
+    padding-top: 3%;
+    //background-color: #61dafb;
+`
+export const Logo = styled.img`
+    width: auto;
+    height: 90%;
+    float: left;
+    cursor: pointer;
+`
+export const LogInWrapper = styled.div`
+    font-family: Regular, sans-serif;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    //align-items: end;
+    float: left;
+    width: 500px;
+    //height: 75%;
+    //background-color: #9bbebe;
+    //padding: 270px 30px;
+    @media (max-width: 1000px) {
+        width: 97vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+`
+export const LogInInput = styled.input`
+    font-family: Regular;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 65%;
+    max-width: 400px;
+    height: 47px;
+    margin-bottom: 15px;
+    font-size: 13px;
+    font-weight: bold;
+    border: 1.5px solid lightgray;
+    border-radius: 10px;
+    padding: 0 10px;
+    color:rgba(151, 151, 151, 1);
+`
+export const LogInButton = styled.div`
+    width: 65%;
+    max-width: 400px;
+    height: 45px;
+    margin-top: 20px;
+    font-family: Regular;
+    background-color: #2643A9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 15px;
+    font-weight: bold;
+    border: 1.5px solid #2643A9;
+    border-radius: 10px;
+    padding: 0 10px;
+    &:hover {
+        cursor: pointer;
+    }
+`
+export const LogInUnderWrapper = styled.div`
+    width: 69%;
+    max-width: 400px;
+    margin-top: 15px;
+    font-size: 11px;
+    font-weight: bold;
+`
+export const SignInButton = styled.div`
+    float: left;
+    &:hover {
+        cursor: pointer;
+    }
+`
+export const FindButton = styled.div`
+    float: right;
+`
+export const ImgWrapper = styled.div`
+    width: 450px;
+    height: 300px;
+    //margin: 0 0 0 50px;
+    @media (max-width: 1000px) {
+        display: none;
+    }
+`
+export const RightImg = styled.img`
+    width: 100%;
+    overflow: hidden;
+`
+export const MiddleWrapper = styled.div`
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 70%;
+    //background-color: gray;
+`
